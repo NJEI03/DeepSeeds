e48922cbe9e85f1b7c24e5f342846114b96e3781
Agentic-JJ-Web3 <njei.private@gmail.com>
2025-07-23 14:45:27 +0100

    Created a temperature conversion

--- 2.0 Software 1.0/1.0 Python Basic/Day 1/variable.py
+++ 2.0 Software 1.0/1.0 Python Basic/Day 1/variable.py
@@ -11,3 +11,7 @@ fav_meal=input('what is ur fav meal')
 
 print('A brief biography')
 print(f'I am called {name} and I am {age} years old.I school at {school} and I am a {job}. About food...I really love {fav_meal} ')
+
+temp_in_celcius = input('What is the temperature in celcius? ')
+temp_in_fahrenheit = (float(temp_in_celcius) * 9/5) + 32
+print(f'The temperature in Fahrenheit is {temp_in_fahrenheit}°F')
