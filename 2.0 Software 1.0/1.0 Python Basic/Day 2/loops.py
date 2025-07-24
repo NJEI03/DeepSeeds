eb7e4b89e20f383ade1710eb8bd29b7c010535f2
Agentic-JJ-Web3 <njei.private@gmail.com>
2025-07-24 14:57:12 +0100

    Finished working on loops behaviour like breaking and continue

--- 2.0 Software 1.0/1.0 Python Basic/Day 2/loops.py
+++ 2.0 Software 1.0/1.0 Python Basic/Day 2/loops.py
@@ -3,23 +3,52 @@
 #2. condition
 #3. increment or decrement
 
-names=['Will', 'JJ','Gita','Kilian','Paul', 'Mbom' 'jj mbom']
+# names=['Will', 'JJ','Gita','Kilian','Paul', 'Mbom' 'jj mbom']
+# # for name in names:
+# count = 0
 # for name in names:
-count = 0
-for name in names:
-    print(f'{count + 1}. {name}')
-    count += 1
-    # if name.endswith('mbom'):
-    #     print(f' We don catch you {name}')
-    # else:
-    #     print(f'welcome to the party : {name}')
+#     print(f'{count + 1}. {name}')
+#     count += 1
+#     # if name.endswith('mbom'):
+#     #     print(f' We don catch you {name}')
+#     # else:
+#     #     print(f'welcome to the party : {name}')
 
-#Range(stop)
-for i in range(5):
-    print(i)
-#Range(start, stop)
-for i in range(2, 7):
-    print(i)
+# #Range(stop)
+# # for i in range(5):
+# #     print(i)
+# # #Range(start, stop)
+# # for i in range(2, 7):
+# #     print(i)
 
-while True:
-    print('madness')
+# count =1
+# while count <= 5:
+
+#     print(f'Count is:{count}')
+#     count += 1
+
+#     user_input = ''
+#     while user_input != 'quit':
+#         user_input = input('Enter "quit" to exit: ')
+
+#         if user_input!= 'quit':
+#          print(f'You have entered:{user_input}')
+#         else:
+#          print('Exiting the loop...')
+
+# print('Goodbye!')
+
+#Break existing loop completely
+print('Finding the first even number:')
+for number in range(1,10):
+    if number % 2 ==0:
+        print(f'Found even number:{number}')
+        break
+    print(f'{number} is odd')
+
+#Continue - skip next iteration
+print('\nPrinting only odd numbers')
+for number in range(1,10):
+    if number % 2 == 0:
+        continue # skip even numbers
+    print(f'Odd number: {number}') 
